02bcee565a5b5ae570f47de794a46de167a21c9a
adanmalik7909 <adanmalik7909@gmail.com>
2025-08-26 15:22:16 +0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Adan's Medical Chatbot",
+    version="0.1.0",
+    author="Malik Adan Khurshid",
+    author_email="adanmalik7909@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    )
+
